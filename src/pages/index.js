8691ea7e3553d90add72483e1e81f3a8255d18d4
Rukshan <arkruka@gmail.com>
2023-01-02 09:48:22 +0530

    new home page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <div className="hero" style={{ height: "25rem" }}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div className="row">
+          <div className="col col--8">
+            <h1 className="hero__title">Documentation</h1>
+            <p className="hero__subtitle">
+              Learn how to setup FHIR Auth and more technical details
+            </p>
+            <div>
+              <button className="button button--primary button-flat button--lg">
+                Get Started
+              </button>
+            </div>
+          </div>
+          <div className="col col--4">
+            <img src="/img/book.png" />
+          </div>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
